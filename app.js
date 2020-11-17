2432ba8955d2ccad42c23280b9952cbaa47d2431
Ann Markham <ann.markham@outlook.com>
2020-11-17 14:47:29 -0700

    add more questiosn and functions

--- app.js
+++ app.js
@@ -1,13 +1,86 @@
 const inquirer = require('inquirer');
-inquirer
-  .prompt([
+
+const promptUser = () => {
+  return inquirer.prompt([
     {
       tupe: 'input',
       name: 'name',
       message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    }
+  ]);
+};
+
+
+const promptProject = portfolioData => {
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  console.log(`
+  ==================
+  Add a New project
+  ==================  
+  `);
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a descripton of the project(Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'With which programs did you build this?(Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: 'false'
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: 'false;'
     }
   ])
-  .then(answers => console.log(answers));
+    .then(projectData => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
+};
+
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
+
 
 // const fs = require('fs');
 // const generatePage = require('./src/page-template.js');
@@ -27,3 +100,5 @@ inquirer
 
 
 
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// })
